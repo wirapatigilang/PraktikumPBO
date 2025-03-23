b0073ea46ed06094e414706aa0992afc171936a3
wirapatigilang <wirapatigilang@gmail.com>
2025-03-23 07:34:19 +0700

    Add Java files

---
+++ Pertemuan5/MBangun.java
@@ -0,0 +1,11 @@
+
+public class MBangun {
+    public static void main(String[] args) {
+        Persegi P1 = new Persegi("Merah", "Putih", 5);
+        Lingkaran L1 = new Lingkaran(3, "Biru", "Putih");
+    
+        P1.printInfo();
+        L1.printInfo();
+    
+    }   
+}
